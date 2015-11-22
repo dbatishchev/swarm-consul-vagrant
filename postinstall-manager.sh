fd3b81dd2d9a482c10f2df4ed6f1cea2fef361a0
dbatishchev <dimitrybatishchev@gmail.com>
2015-11-22 18:19:06 +0300

    add fixes

--- postinstall-manager.sh
+++ postinstall-manager.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
 sed -i "s/10.0.2.3/8.8.8.8/g" /etc/resolv.conf
-apt-get update && apt-get upgrade
+apt-get update --yes && apt-get upgrade --yes
+apt-get install python-pip python-dev build-essential --yes | sudo sh
 
 # Install docker
 curl -sSL https://get.docker.com/ | sudo sh
 service docker stop
 sed -i "s/DOCKER_OPTS=/DOCKER_OPTS='-H tcp:\/\/0.0.0.0:2375 -H unix:///var/run/docker.sock'/g" /etc/init/docker.conf
 service docker start
+
+pip install docker-compose
 
 export DOCKER_HOST=tcp://0.0.0.0:2375
 echo "export DOCKER_HOST=tcp://0.0.0.0:2375" >> /root/.bashrc
