06bc4cfea511fa861fdf584dae7358e6416e81fc
Dmitrii Batishchev <dimitrybatishchev@gmail.com>
2015-11-22 22:31:19 -0800

    add docker-compose provision

--- postinstall-manager.sh
+++ postinstall-manager.sh
@@ -2,7 +2,6 @@
 
 sed -i "s/10.0.2.3/8.8.8.8/g" /etc/resolv.conf
 apt-get update --yes && apt-get upgrade --yes
-apt-get install python-pip python-dev build-essential --yes | sudo sh
 
 # Install docker
 curl -sSL https://get.docker.com/ | sudo sh
@@ -10,7 +9,9 @@ service docker stop
 sed -i "s/DOCKER_OPTS=/DOCKER_OPTS='-H tcp:\/\/0.0.0.0:2375 -H unix:///var/run/docker.sock'/g" /etc/init/docker.conf
 service docker start
 
-pip install docker-compose
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose | sudo sh
+chmod +x /usr/local/bin/docker-compose | sudo sh
 
 export DOCKER_HOST=tcp://0.0.0.0:2375
 echo "export DOCKER_HOST=tcp://0.0.0.0:2375" >> /root/.bashrc
--- postinstall-node.sh
+++ postinstall-node.sh
@@ -3,10 +3,13 @@
 sed -i "s/10.0.2.3/8.8.8.8/g" /etc/resolv.conf
 apt-get update --yes && apt-get upgrade --yes | sudo sh
 
+
 # Install docker
 curl -sSL https://get.docker.com/ | sudo sh
 service docker stop
 sed -i "s/DOCKER_OPTS=/DOCKER_OPTS='-H tcp:\/\/0.0.0.0:2375'/g -H unix:///var/run/docker.sock" /etc/init/docker.conf
 service docker start
 
-pip install docker-compose
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose | sudo sh
+chmod +x /usr/local/bin/docker-compose | sudo sh
